261d5f6fceac9ad8c87c4954b586290edc6b93d9
Vanderclin Rocha <vanderclinrocha@gmail.com>
2022-12-01 18:20:19 -0300

    New Update Theme

--- app/src/main/java/com/magicdeveloper/advertisementunity/MainActivity.java
+++ app/src/main/java/com/magicdeveloper/advertisementunity/MainActivity.java
@@ -28,7 +28,6 @@ public class MainActivity extends AppCompatActivity implements IUnityAdsInitiali
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         // Initialize the SDK:
         UnityAds.initialize(getApplicationContext(), GameID, TestMode, this);
